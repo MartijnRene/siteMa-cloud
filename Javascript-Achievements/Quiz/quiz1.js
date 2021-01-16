8a9a5d963d90aa835a5d945aba1f5bc682d6f8bd
Martijn <martijnrenevdmeer@gmail.com>
2021-01-16 19:50:46 +0100

    added Meet the Dom, Quiz, Device Orientation

---
+++ Javascript-Achievements/Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.dewestkrant.nl/wp-content/uploads/2017/05/IMG_0674-e1496049647937.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "In welke plaats is het Mediacollege?",
+				"answers": [{
+						"answer": "Amsterdam",
+						"feedback": true
+					},
+					{
+						"answer": "Purmerend",
+						"feedback": false
+					},
+					{
+						"answer": "Utrecht",
+						"feedback": false
+					},
+					{
+						"answer": "Haarlem",
+						"feedback": false
+					},
+					{
+						"answer": "Den Haag",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Was is de beste Software Development klas?",
+				"answers": [{
+						"answer": "SD1A",
+						"feedback": true
+					},
+					{
+						"answer": "SD1B",
+						"feedback": false
+					},
+					{
+						"answer": "SD1C",
+						"feedback": false
+					},
+					{
+						"answer": "SD1D",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Italianen staan bekend om?",
+				"answers": [{
+						"answer": "spaghetti",
+						"feedback": true
+					},
+					{
+						"answer": "pasta",
+						"feedback": true
+					},
+					{
+						"answer": "stokbrood",
+						"feedback": false
+					},
+					{
+						"answer": "suikerklontjes",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
---
+++ Javascript-Achievements/Quiz/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Math Trivia  1",
+        "imageURI": "https://preview.redd.it/ftke3yhj5ga41.png?auto=webp&s=49b184c62e52155d8b1b50856107a1a6d4f4e20d"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "What is the number after 7",
+				"answers": [{
+						"answer": "6",
+						"feedback": false
+					},
+					{
+						"answer": "7",
+						"feedback": false
+					},
+					{
+						"answer": "8",
+						"feedback": true
+					},
+					{
+						"answer": "9",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "5 - 4 = ?",
+				"answers": [{
+						"answer": "0",
+						"feedback": false
+					},
+					{
+						"answer": "1",
+						"feedback": true
+					},
+					{
+						"answer": "2",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "What is 0 to the power of 0?",
+				"answers": [{
+						"answer": "0",
+						"feedback": false
+					},
+					{
+						"answer": "1",
+						"feedback": true
+					},
+					{
+						"answer": "2",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
